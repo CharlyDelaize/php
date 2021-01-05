7ccfef47c7dadfc66a60d60e1bea7eb55bee2e2d
CharlyDelaize <charly.delaize@lepoles.com>
2021-01-05 22:21:12 +0100

    suite cours php

--- entrainement.php
+++ entrainement.php
@@ -228,12 +228,274 @@ switch ($couleur)
 // EXERCICE : Reproduisez la meme chose que le switch avec des conditions
 
 $couleur = 'vert';
+
 if ($couleur == 'bleu'){
     echo 'vous aimez le bleu <br>';
-}elseif($couleur == 'rouge'){
-    echo 'vous aimez le rouge <br>';
-}elseif($couleur == 'vert'){
-    echo 'vous aimez le vert <br>';
-}else{
-    echo 'vous n\'aimez ni le bleu, ni le rouge... <br>';
 }
+elseif($couleur == 'rouge'){
+    echo 'vous aimez le rouge <br>';
+}
+elseif($couleur == 'vert'){
+    echo 'vous aimez le vert <br>';
+}
+else{
+    echo 'aucune des conditions n\'est bonne <br>';
+}
+//--------------------------------------------------
+echo '<hr><h2>Fonctions Prédéfinies</h2>';
+echo '<br>Date :<br>';
+print date("d/m/Y");
+echo '<br>';
+//---------
+echo '<hr><h2>Fonctions Prédéfinies : Traitement des chaines (iconv_strlen, strpos, substr) </h2>';
+$email1 = "toto@gmail.com";
+echo strpos($email1, "@");
+//------------------------
+$email2 = "bonjour";
+echo strpos($email2, "@");
+var_dump(strpos($email2, "@"));
+/*
+strpos() est une fonction prédéfinie permettant de trouver la position d'un caractère dans une chaine de caractères
+Succès : integer (int)
+Echec : boolean false (boolean)
+Arguments :
+1. Nous devons lui fournir la chaine dans laquelle nous devons chercher
+2. Nous devons lui fournir l'information à chercher
+*/
+//-------
+$phrase = "Ceci est une chaine de caractere";
+echo iconv_strlen($phrase);
+/*
+iconv_strlen() est une fonction prédéfinie permettant de retourner la taille d'une chaine :
+Succès : int
+Echec : boolean false
+Argument :
+Nous devons lui fournir la chaine dont laquelle nous souhaitons connaitre la taille
+*/
+//-------
+$texte = "La situation est inchangée ce mardi avec une dépression en Méditerranée et un anticyclone en Mer du Nord, dirigeant un flux de nord-est froid";
+echo substr($texte, 0, 10); "<a href=''>Lire la suite</a>";
+/*
+substr() est une fonction prédéfinie permettant de retourner une partie de la chaine
+Succès : string
+Echec : Boolean false
+Arguments : boolean false
+1. Nous devons lui fournir la chaine que l'on souhaite couper
+2. Nous devons lui préciser la position de début 
+3. Nous devons lui préciser la position de fin
+*/
+//-------
+echo '<hr><h2>Fonctions Utilisateurs</h2>';
+// Les fonctions qui ne sont pas définis dans le langage sont déclaré et éxecuté par l'utilisateur
+function separation() // Déclaration d'une fonction prévue pour ne pas laisser d'arguments
+{
+    echo "<hr><hr><hr>";
+}
+
+separation();
+//----------
+// Fonction avec arguments :
+function bonjour($qui)
+{
+    return "Bonjour $qui <br>";
+}
+$prenom = 'Sara';
+//execution
+// La fonction bonjour() attend toujours un argument qui est obligatoire pour l'execution
+echo bonjour($prenom); // L'argument peut etre une variable
+echo bonjour('david'); // Mais aussi une chaine
+//---------------------------------------------
+function appliqueTva($nombre)
+{
+    return $nombre*1.2;
+}
+echo appliqueTva(100) . '<br>';
+//--- Exercice : Pourriez-cous améliorer cette fonction afin que l'on puisse calculer un nombre avec les taux de notre choix(19.6%, 5.5%, 7%)
+function appliqueTva2($nombre, $tva = 1.196)
+{
+    return $nombre*$tva;
+}
+echo '100€ avec une TVA à 7% font ' . appliqueTva2(100) . '<br>';
+//---------------------------------------------------------------
+meteo("hiver", "2");
+function meteo($saison, $temperature)
+{
+    echo "Nous sommes en $saison et il fait $temperature degré(s) <br>";
+}
+
+//Exercice gérez le S de degrés en fonction de la température
+exoMeteo("hiver", "1");
+function exoMeteo($saison, $temperature)
+{
+    if($temperature >= -1 && $temperature <= 1){
+        echo "Nous sommes en $saison et il fait $temperature degré <br>";
+    }else{
+        echo "Nous sommes en $saison et il fait $temperature degrés <br>";
+    }
+} 
+//----------------------------------------------------------------------
+function jourSemaine()
+{
+    $jour = "Lundi <br>"; // variable local
+    return $jour; // Une fonction peut retourner qq chose (à ce moment là on quitte la fonction)
+    echo 'coucou';// /!\ CETTE LIGNE NE SORTIRA JAMAIS CAR IL Y A UN RETURN AVANT
+}
+//echo $jour; // ne s'affiche pas car $jour est une variable locale
+echo jourSemaine(); // affiche 'Lundi' et pas le 'coucou' qui lui est après le return
+//-----------------------------------------------------------------------------------
+$pays = 'Maroc';
+
+function affichePays()
+{
+    global $pays;
+    echo $pays;
+}
+affichePays();
+//------------
+function facultatif() // fonction avec argument facultatif
+{
+    print "<pre>"; print_r(func_get_args()); print "<pre>";
+    $array = func_get_args();
+    foreach($array as $indice => $element) // func_get_args() permet d'obtenir un tableau avec les arguments passés.
+    {
+        echo $indice, "->", $element, "<br>";
+    }
+}
+facultatif();
+facultatif("France", "Italie", "Algérie", 7);
+facultatif("allo");
+//-----------------
+//On précise en amont le type obligatoire des valeurs entrantes des arguments
+function identite(string $nom, int $age)
+{
+    return $nom . ' a ' . $age . ' ans' . '<br>';
+}
+echo identite('Hocine', "37");
+//----------------------------
+// On précise en amont la valeur de retour que doit ressortire la fonction
+function isAdult(int $age) : bool
+{
+    return $age >= 18;
+}
+var_dump(isAdult(7));
+//-------------------
+echo '<hr><h2>Structures itérative : Boucle</h2>';
+$i = 0; // valeur de départ
+while($i<3) // tant que $i est inférieur à 3
+{
+    echo "$i---"; // affiche moi $i
+    $i++; // ceci est une forme contractée de : $i = $i + 1; c'est l'incrémentation du compteur et elle est effectué à chaque tour de boucle
+}
+// Affiche "0---1---2---"
+echo '<br>';
+// Exercice : Faites en sorte de ne pas avoir de tiret à la fin : 0---1---2
+
+$j = 0; // valeur de départ
+while($j<3) // tant que $i est inférieur à 3
+{
+    if($j < 2)
+    { // Je rentre 2 fois tant que $j est inférieur à 2
+        echo "$j---";
+    }else
+    { // Je rentre une fois quand j est égal à 2
+        echo "$j";
+    }
+    $j++;
+}echo '<br>';
+//-----------
+// Boucle for
+for($j=0; $j < 15; $j++)
+{
+    print $j . '<br>';
+}
+// Exercice : afficher 30 options via une boucle
+// echo '<select>';
+// echo "<option>$k</option>";
+// echo "<option>$k</option>";
+// echo '</select>';
+//Correction
+echo '<select>';
+for($k=1; $k <= 30; $k++)
+{
+    echo "<option>$k</option>";
+}
+echo '</select>';
+echo '<br>';
+// --------------
+// Exercice : faites une boucle qui affiche de 0 à 9 sur la meme ligne dans un tableau
+
+echo '<table border=1><tr>';
+for($l=0; $l <= 9; $l++)
+{
+    echo "<td>$l</td>";
+}
+echo '</tr></table><br>';
+// Exercice : faites la meme chose en allant de 0 à 99 sur plusieurs lignes sans faire 10 boucles
+
+echo '<table border=1>';
+    for($m=1 ; $m <= 1; $m++){
+
+        echo "<tr>";
+        for($o=0; $o <= 9; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=10; $o <= 19; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=20; $o <= 29; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=30; $o <= 39; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=40; $o <= 49; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=50; $o <= 59; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=60; $o <= 69; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=70; $o <= 79; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=80; $o <= 89; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+        echo "</tr>";
+        for($o=90; $o <= 99; $o++){
+            echo "<td>";
+            echo $o;
+            echo "</td>";
+        }
+    }
+echo '</table>';
+
+?>
